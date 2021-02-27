66a8e876d6b4344f0803f27402760f790ace2f0e
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-27 17:54:13 -0300

    store patterns

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,26 +1,68 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
-
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import {
+  concat,
+  fromEvent,
+  interval,
+  noop,
+  observable,
+  Observable,
+  of,
+  timer,
+  merge,
+  Subject,
+  BehaviorSubject,
+  AsyncSubject,
+  ReplaySubject,
+} from "rxjs";
+import { delayWhen, filter, map, take, timeout } from "rxjs/operators";
+import { createHttpObservable } from "../common/util";
 
 @Component({
-    selector: 'about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
+  ngOnInit() {
+    // const subject = new Subject();
+    // const series$ = subject.asObservable();
+    // series$.subscribe((val) => console.log("early sub:", val));
+    // subject.next(1);
+    // subject.next(2);
+    // subject.next(3);
+    // subject.complete();
 
-    ngOnInit() {
+    // const subject = new BehaviorSubject(0);
+    // const series$ = subject.asObservable();
+    // series$.subscribe((val) => console.log("early sub:", val));
+    // subject.next(1);
+    // subject.next(2);
+    // subject.next(3);
+    // // subject.complete();
+    // setTimeout(() => {
+    //   series$.subscribe((val) => console.log("late sub:", val));
+    //   subject.next(4);
+    // }, 3000);
 
+    // const subject = new AsyncSubject();
+    // const series$ = subject.asObservable();
+    // series$.subscribe((val) => console.log("first sub:", val));
+    // subject.next(1);
+    // subject.next(2);
+    // subject.next(3);
+    // subject.complete();
 
-    }
+    // const subject = new ReplaySubject();
+    // const series$ = subject.asObservable();
+    // series$.subscribe((val) => console.log("first sub:", val));
+    // subject.next(1);
+    // subject.next(2);
+    // subject.next(3);
+    // //subject.complete();
 
-
+    // setTimeout(() => {
+    //   series$.subscribe((val) => console.log("second sub:", val));
+    //   subject.next(5)
+    // }, 3000);
+  }
 }
-
-
-
-
-
-
